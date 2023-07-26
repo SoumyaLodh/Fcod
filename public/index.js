404ff8b574d29caf88059086bd1c05ad1fc976c0
Soumya Lodh <soumya.lodh206@gmail.com>
2023-07-26 13:45:06 +0530

    Made the site responsive

--- public/index.js
+++ public/index.js
@@ -17,34 +17,34 @@ const testingUserData = {
 
 // fetchFriendList();
 
-// function addUserPopupOpen() {
-//   const ele = document.querySelector("#add-friend-pop-up");
-//   ele.classList.remove("hidden-popup");
-// }
+function addUserPopupOpen() {
+  const ele = document.querySelector("#add-friend-pop-up");
+  ele.classList.remove("hidden-popup");
+}
 
-// function addUserPopupClose() {
-//   const ele = document.querySelector("#add-friend-pop-up");
-//   ele.classList.add("hidden-popup");
-// }
+function addUserPopupClose() {
+  const ele = document.querySelector("#add-friend-pop-up");
+  ele.classList.add("hidden-popup");
+}
 
-// function fetchFriendList() {
-//   const list = document.getElementById("dm-list");
-//   for (let i = 0; i < testingUserData.friendList.ID.length; i++) {
-//     let element = document.createElement("div");
-//     element.innerHTML = `
-//     <div class="user-btn">
-//     <div class="user-image">
-//         <br />
-//         <img src="./src/img/logo/discord.png" alt="">
-//     </div>
-//     <div class="user-userid">
-//         <br />
-//         <h2>${testingUserData.friendList.ID[i]}</h2>
-//     </div>
-//   </div>
-//     `;
-//     list.append(element);
-//   }
-// }
+function fetchFriendList() {
+  const list = document.getElementById("dm-list");
+  for (let i = 0; i < testingUserData.friendList.ID.length; i++) {
+    let element = document.createElement("div");
+    element.innerHTML = `
+    <div class="user-btn">
+    <div class="user-image">
+        <br />
+        <img src="./src/img/logo/discord.png" alt="">
+    </div>
+    <div class="user-userid">
+        <br />
+        <h2>${testingUserData.friendList.ID[i]}</h2>
+    </div>
+  </div>
+    `;
+    list.append(element);
+  }
+}
 
 function fetchIncomingRequest() {}
